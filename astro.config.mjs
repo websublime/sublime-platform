481eb3d2a99455650daf26c51c68f0b5c37a115e
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2022-10-04 17:15:07 +0100

    feat: test globals

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,17 +10,16 @@ export default defineConfig({
       hooks: {
         'astro:config:setup': ({ injectScript }) => {
           const { API_URL = 'http://localhost', NODE_ENV = 'production' } = process.env;
-          injectScript(
-            'page',
-            `
-            import { bootGlobals } from "@websublime/ws-globals";
-            bootGlobals({apiUrl: ${API_URL}, env: ${NODE_ENV}});
-            `
-          );
+          injectScript('page', `import { bootGlobals } from "@websublime/ws-globals";bootGlobals({apiUrl: "${API_URL}", env: "${NODE_ENV}"});`);
         }
       },
       name: '@sublime/globals'
     }
   ],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@websublime/ws-essential']
+    }
+  },
   output: 'server'
 });
